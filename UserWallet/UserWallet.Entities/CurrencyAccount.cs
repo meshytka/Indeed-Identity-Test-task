215982026c2131aed52c0808c0d47a88d6be59a5
Adasda <asdasdas>
2020-11-06 19:02:41 +0300

    UserWallet: Add Entities

---
+++ UserWallet/UserWallet.Entities/CurrencyAccount.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserWallet.Entities
+{
+    public enum Currency
+    {
+        EUR,
+        USD,
+        JPY,
+        BGN,
+        CZK,
+        DKK,
+        GBP,
+        HUF,
+        PLN,
+        RON,
+        SEK,
+        CHF,
+        ISK,
+        NOK,
+        HRK,
+        RUB,
+        TRY,
+        AUD,
+        BRL,
+        CAD,
+        CNY,
+        HKD,
+        IDR,
+        ILS,
+        INR,
+        KRW,
+        MXN,
+        MYR,
+        NZD,
+        PHP,
+        SGD,
+        THB,
+        ZAR
+    }
+
+    public class CurrencyAccount
+    {
+        public Guid CurrencyAccountId { get; set; }
+        public Currency CurrencyType { get; set; }
+        public decimal Value { get; set; }
+
+        public Guid WalletId { get; set; }
+        public Wallet Wallet { get; set; }
+    }
+}
---
+++ UserWallet/UserWallet.Entities/Wallet.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UserWallet.Entities
+{
+    public class Wallet
+    {
+        public Guid ClientId { get; set; }
+        public List<CurrencyAccount> CurrencyAccounts { get; set; }
+    }
+}
