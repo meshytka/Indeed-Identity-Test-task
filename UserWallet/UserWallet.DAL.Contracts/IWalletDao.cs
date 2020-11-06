fbf9f21ccad4a05781506411f34cdbb76dc5350f
Adasda <asdasdas>
2020-11-06 19:03:54 +0300

    Add contracts

---
+++ UserWallet/UserWallet.DAL.Contracts/IWalletDao.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UserWallet.Entities;
+
+namespace UserWallet.DAL.Contracts
+{
+    public interface IWalletDao
+    {
+        public Wallet GetUserWallet(Guid id);
+        public void SaveUserWallet(Wallet wallet);
+    }
+}
